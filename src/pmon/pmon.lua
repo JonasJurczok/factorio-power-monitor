3697dd7185c8edb0930181c39131256af5131c0f
Jonas Jurczok <jonas.jurczok@akelius.de>
2018-11-26 20:28:29 +0100

    #1 add first debug output

--- src/pmon/pmon.lua
+++ src/pmon/pmon.lua
@@ -6,6 +6,11 @@ end
 
 function pmon.on_tick(event)
     if (event.tick % 10 == 0) then
-        event.player.print("Energy: " .. player.selected.energy .. "J" )
+        for _, player in pairs(game.players) do
+            if (player.selected) then
+
+                player.print("Energy: " .. player.selected.energy .. "J" )
+            end
+        end
     end
 end
